f28c4004f9dc6bcfb4026ab8f017bf33618693dd
gradybknight <grady.knight@gmail.com>
2020-05-16 17:34:18 -0400

    update

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req,res) => {
-
-	res.send('hello world from express');
+	res.send('hello world from express linked to github');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
