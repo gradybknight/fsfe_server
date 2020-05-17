7444debe0f8fe00947487d3f413b6b2352d97b5d
gradybknight <grady.knight@gmail.com>
2020-05-16 22:27:02 -0400

    new endpoint

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ app.get('/', (req,res) => {
 	res.send('hello world from express linked to github');
 });
 
+app.get('/demo', (req,res)=> {
+	res.set('X-full-stack', '4life');
+	res.status(418);
+	res.send('Coffee is better');
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
